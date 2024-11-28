b3e372821a8a44343c4c5478a1cf871d238ccac8
unknown <rutzen@proton.me>
2024-11-27 21:33:44 -0300

    ss

--- produtos/forms.py
+++ produtos/forms.py
@@ -17,11 +17,25 @@ class ProdutoForm(forms.ModelForm):
                   'preco': 'Preço do produto',
                   'estoque': 'Estoque',
                   }
+    
+    def clean_nome(self):
+        nome = self.cleaned_data.get('nome')
+        if len(nome) < 3:
+            raise forms.ValidationError('o nome deve ter no mínimo 3 caracteres')
+        return nome
+    
+    def clean_estoque(self):
+        estoque = self.cleaned_data.get('estoque')
+        if estoque == 0:
+            raise forms.ValidationError('estoque vazio')
+        return estoque
         
 
 
 class FormularioForm(forms.Form):
     forma = forms.CharField(max_length=100)
+
+
         
 
         
